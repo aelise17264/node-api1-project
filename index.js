bc79e925136e126f35e2206371466577b0b595a4
unknown <aelise17264@gmail.com>
2020-09-28 16:19:22 -0600

    put and delete working

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ server.post('/users', (req, res) => {
     if (!name || !bio){
         res.status(400).json({errorMessage: "Please provide name and bio for the user"});
     }
-   
     res.status(201).json({data: users})
     res.status(500).json({errorMessage: "There was an error while saving the user to the database"});
 
@@ -48,9 +47,7 @@ server.get('/users/:id', (req, res) => {
     const user = users.filter( thisUser => thisUser.id === id)
     if(!user){
         Object.assign(user)
-        res.status(200).json({data: user})
-
-            
+        res.status(200).json({data: user})            
         }else{
             res.status(400).json({
                 errorMessage: 'The user with the specified ID does not exist'})
@@ -58,6 +55,36 @@ server.get('/users/:id', (req, res) => {
 
 });
 
+server.delete('/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    users = users.filter( thisUser => thisUser.id !== id)
+    res.status(200).json({data: users})
+    res.status(404).json({errorMessage: "The user with the specified ID does not exist."})
+    res.status(500).json({errorMessage: "The user could not be removed."})
+})
+
+server.put('/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const changes = req.body;
+    const {name, bio} = req.body;
+    const thisUser = users.find(item => item.id === id);
+    // if(thisUser){
+        
+        if(!name || !bio){
+            res.status(400).json({errorMessage: "Please provide name and bio for the user"});
+       // }
+        
+    }else {
+        Object.assign(thisUser, changes);
+        res.status(200).json({data: users});
+        }
+       
+        res.status(404).json({ errorMessage: "The user with the specified ID does not exist." })
+
+    
+   res.status(500).json({errorMessage: "The user information could not be modified."})
+    
+})
 
 const port = 3000
 
