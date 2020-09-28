e0c9886f9c5a2c23182196f9eb6c0d8256de17d8
unknown <aelise17264@gmail.com>
2020-09-28 14:45:12 -0600

    first get & post finished

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 
+let users = [
+    {id: 1,
+     name: 'Anna',
+    bio: 'Full Stack Web Developer'},
+    
+    {id: 2,
+    name: 'Ian',
+    bio: 'Animation, director of photography'},
+    
+    {id: 3,
+    name: 'Candice',
+    bio: 'Teacher'
+    }
+]
+
 const express = require('express');
 
 const server = express();
@@ -8,6 +23,19 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.status(200).json({hello: 'server working'})
 })
+
+server.get('/users', (req, res) => {
+    res.status(200).json({data: users})
+})
+
+let nextId = 4;
+
+server.post('/users', (req, res) => {
+    const data = req.body
+    users.push({id: nextId++, ...data})
+    res.status(201).json({data: users})
+})
+
 
 const port = 3000
 
