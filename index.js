ec817a18d622d4ab69e3e62528c9415056ffaaf3
unknown <aelise17264@gmail.com>
2020-09-28 14:02:44 -0600

    basic layout of api, getting module not found error

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+
+// const server = express();
+
+// server.use(express.json());
+
+// server.get('/', (req, res) => {
+//     res.status(200).json({hello: 'server working'})
+// })
+
+// const port = 3000
+
+// server.listen(port, () => console.log('server running!'))
